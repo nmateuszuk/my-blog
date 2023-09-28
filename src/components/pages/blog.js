81cd5ecab08e3acacc63205d56f1bbed7c0c8825
nmateuszuk <n.mateuszuk@gmail.com>
2023-09-28 10:48:48 -0500

    Blog is now on blog

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,25 +1,12 @@
 import React from 'react'
+import Posts from '../posts/Posts'
 
-function Blog(){
+function Blog(){   return (
+  <div className="blog">
+    <Posts />
+  </div>
+);
 
-    const [data, setData] = React.useState(null);
-
-    React.useEffect(() => {
-        fetch("/api/post")
-          .then((response) => response.json())
-          .then((data) => setData(data))
-      }, []);
-
-      return (
-        <div className="App">
-          <header className="App-header">
-            <p>{!data ? "Loading..." : data["title"]}</p>
-            <p>{!data ? "Loading..." : data["body"]}</p>
-            <p>{!data ? "Loading..." : data["createdAt"]}</p>
-            <p>{!data ? "Loading..." : data["id"]}</p>
-          </header>
-        </div>
-      );
 }
 
 export default Blog;
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Posts from '../posts/Posts'
 import Sidebar from '../sidebar/Sidebar';
 
 function Home(){
     return (
           <div className="home">
-            <Posts />
             <Sidebar />
           </div>
       );
