cbab0303339ea76b1c77a683ce32c2131d237141
nmateuszuk <n.mateuszuk@gmail.com>
2023-09-28 07:19:27 -0500

    Post added

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
 function Blog(){
-    return <div>Blog</div>
+
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch("/api/post")
+          .then((response) => response.json())
+          .then((data) => setData(data))
+      }, []);
+
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>{!data ? "Loading..." : data["title"]}</p>
+            <p>{!data ? "Loading..." : data["body"]}</p>
+            <p>{!data ? "Loading..." : data["createdAt"]}</p>
+            <p>{!data ? "Loading..." : data["id"]}</p>
+          </header>
+        </div>
+      );
 }
 
 export default Blog;
