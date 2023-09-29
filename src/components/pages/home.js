8e80cf1ed7607593e461cbb7047c109d02512708
nmateuszuk <n.mateuszuk@gmail.com>
2023-09-29 12:47:52 -0500

    Masonery grid initial

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,16 +1,68 @@
 import React from 'react'
 import Sidebar from '../sidebar/Sidebar';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Masonry from '@mui/lab/Masonry';
+import './home.css'
+
+// Dome dummy content
+const PHOTOS = [
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/2516406/pexels-photo-2516406.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/3698534/pexels-photo-3698534.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/3047993/pexels-photo-3047993.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/8979525/pexels-photo-8979525.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/8979525/pexels-photo-8979525.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/8979525/pexels-photo-8979525.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+];
 
 
-export default function Home(){
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(0.5),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function Home(){
     return (
-      
-    <section className="home">
+      <section className="home">
       <section>
         <Sidebar/>
       </section>
-      <section className="container home">
+        <section className="container home">
+        <Box sx={{ width: 1200, height: 300 }}>
+          <Masonry columns={3} spacing={3}>
+            {PHOTOS.map((photo) => (
+              <li className={`photo-item`}>
+              <img src={photo.imageUrl} alt="" />
+            </li>
+            ))}
+          </Masonry>
+        </Box>
       </section>
     </section>)
 
+
 }
+
+export default Home;
