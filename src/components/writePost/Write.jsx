584a0735576595af7dc843ca9733d86f0b22e1ca
nmateuszuk <n.mateuszuk@gmail.com>
2023-09-29 17:07:52 -0500

    Working posting

--- src/components/writePost/Write.jsx
+++ src/components/writePost/Write.jsx
@@ -1,6 +1,47 @@
+import React from "react";
 import "./write.css";
+import { useState } from "react";
 
 export default function Write() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [imageLink, setImageLink] = useState("");
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(JSON.stringify({
+      title: title,
+      body: body,
+      imageLink: imageLink,
+    }));
+    try {
+      let res = await fetch("/api/post/write", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: title,
+          body: body,
+          imageLink: imageLink,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setTitle("");
+        setBody("");
+        setImageLink("");
+        setMessage("Post created successfully");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="write">
       <img
@@ -8,30 +49,39 @@ export default function Write() {
         src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
         alt=""
       />
-      <form className="writeForm">
+      <form className="writeForm" onSubmit={handleSubmit}>
+        <div className="message">{message ? <p>{message}</p> : null}</div>
         <div className="writeFormGroup">
-          <label htmlFor="fileInput">
-            <i className="writeIcon fas fa-plus"></i>
-          </label>
           <input id="fileInput" type="file" style={{ display: "none" }} />
           <input
             className="writeInput"
-            placeholder="Title"
             type="text"
+            value={title}
+            placeholder="Title"
             autoFocus={true}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="writeFormGroup">
-          <textarea
+          <input
             className="writeInput writeText"
-            placeholder="Tell your story..."
             type="text"
+            value={body}
+            placeholder="Tell your story..."
             autoFocus={true}
+            onChange={(e) => setBody(e.target.value)}
           />
         </div>
-        <button className="writeSubmit" type="submit">
-          Publish
-        </button>
+        <div>
+          <input
+            className="writeImageLink writeImageLink"
+            type="text"
+            value={imageLink}
+            placeholder="Post image link"
+            onChange={(e) => setImageLink(e.target.value)}
+          />
+        </div>
+        <button type="submit">Create</button>
       </form>
     </div>
   );
